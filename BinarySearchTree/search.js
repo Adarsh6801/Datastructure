58d10fb54ceda217c8c487a93edbd8a088792878
Adarsh <kpadarsh6801@gmail.com>
2023-05-31 11:30:50 +0530

    updated

--- BinarySearchTree/search.js
+++ BinarySearchTree/search.js
@@ -1,57 +1,57 @@
-class Node{
+class Node {
     constructor(value){
         this.value=value;
         this.right=null;
         this.left=null;
     }
 }
-
 class BinarySearchTree{
     constructor(){
         this.root=null;
     }
     isEmpty(){
-        return this.root===null;
+       return this.root===null;
     }
     insert(value){
         const newNode= new Node(value)
         if(this.root===null){
-            this.root=newNode
+            this.root=newNode;
         }else{
             this.insertNode(this.root,newNode)
         }
     }
     insertNode(root,newNode){
-        if(newNode.value<root.value){
+        if(root.value>newNode.value){
             if(root.left===null){
                 root.left=newNode;
             }else{
                 this.insertNode(root.left,newNode)
             }
         }else{
-            if(root.right===null){
+            if(root.right=== null){
                 root.right=newNode;
             }else{
-                this.insertNode(root.right,newNode)
+                this.insertNode(root.right, newNode)
             }
         }
     }
-
     search(root,value){
         if(!root){
             return false;
         }else{
-            if(root.value===value){
+            if(root.value=== value){
                 return true;
+            }else if(value < root.value){
+               return this.search(root.left,value)
             }
-            else if(value<root.value){
-                return this.search(root.left,value)
-            }else{
+            else{
                 return this.search(root.right,value)
             }
         }
     }
 }
+
+
 
 const tree = new BinarySearchTree()
 
